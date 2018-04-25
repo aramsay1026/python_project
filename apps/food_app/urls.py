241976f5917dc0101b5a59193a6e41a3d7913d72
Amy <aramsay1026@gmail.com>
2018-04-25 15:32:47 -0700

    adding to new repo

--- apps/food_app/urls.py
+++ apps/food_app/urls.py
@@ -3,8 +3,15 @@ from . import views
 
 urlpatterns = [
 
-	url(r'^$', views.home), 
-	
-	url(r'^welcome', views.process)
-
+    url(r'^$', views.home), 
+    url(r'^sendtosuccess$', views.process),
+    url(r'^welcome$',views.validation),
+    url(r'^login$',views.login),
+    url(r'^supplier$',views.supplier),
+    url(r'^process_add$',views.addSupplier),
+    url(r'^senddetails$',views.send_details),
+    url(r'^findVolunteer$',views.find_volunteer),
+    url(r'^volunteers$',views.volunteer_home),
+    url(r'^volunteer_join$',views.volunteer_add),
+    url(r'^shelter$',views.shelter),
 ]
